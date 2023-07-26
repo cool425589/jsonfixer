ef29208bc2eda676bf02415c26a1701f4405f097
a94763075 <a94763075@163.com>
2023-07-26 15:25:42 +0800

    [feat] main algorithm

--- jsonfixer/__init__.py
+++ jsonfixer/__init__.py
@@ -1,18 +1,25 @@
-from .json_fixer import fix_json_data
-from .json_utils import load_json_file, save_json_file, pretty_print_json
-from .json_validator import is_json_valid, get_json_errors
-from .json_converter import json_to_csv, json_to_xml
-from .json_analyzer import analyze_json_structure, get_json_properties
+# from .json_fixer import fix_json_data
+# from .json_utils import load_json_file, save_json_file, pretty_print_json
+# from .json_validator import is_json_valid, get_json_errors
+# from .json_converter import json_to_csv, json_to_xml
+# from .json_analyzer import analyze_json_structure, get_json_properties
+
+# __all__ = [
+#     'fix_json_data',
+#     'load_json_file',
+#     'save_json_file',
+#     'pretty_print_json',
+#     'is_json_valid',
+#     'get_json_errors',
+#     'json_to_csv',
+#     'json_to_xml',
+#     'analyze_json_structure',
+#     'get_json_properties'
+# ]
+
+
+from .json_fixer import JsonRepair
 
 __all__ = [
-    'fix_json_data',
-    'load_json_file',
-    'save_json_file',
-    'pretty_print_json',
-    'is_json_valid',
-    'get_json_errors',
-    'json_to_csv',
-    'json_to_xml',
-    'analyze_json_structure',
-    'get_json_properties'
+    'JsonRepair',
 ]
